c61e7ea1c40cb8b4e38fe609f2775004da9daaf1
AlondraMendoza <alondra_mendozab@hotmail.com>
2018-09-05 19:45:33 -0500

    Cambios
    Cambios

--- application/views/capturista/ObtenerModelos.php
+++ application/views/capturista/ObtenerModelos.php
@@ -2,7 +2,7 @@
 <div class="grid" >
     <div data-role="group" data-group-type="one-state">
        <?php if ($modelos->num_rows() == 0) { ?>
-        <label>No hay Productos</label>
+        <label>No hay modelos</label>
         <?php } else { ?>
         <?php foreach ($modelos->result() as $modelo ): ?>
         <?php
